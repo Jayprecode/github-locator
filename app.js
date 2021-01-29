bd5055164253953e77a016596fc0a1d01ddc9b5e
Jayprecode <jayprecode@gmail.com>
2021-01-29 21:37:30 +0100

    Updates

---
+++ app.js
@@ -0,0 +1,30 @@
+//Init Github
+const github = new Github();
+
+//Init UI
+const ui = new UI();
+
+let sU = document.getElementById("searchUser");
+
+//Search Input eventListener
+sU.addEventListener("keyup", (e) => {
+	//Get input text
+	let userTxt = e.target.value;
+	if (userTxt !== "") {
+		//Make http call
+		github.getUser(userTxt).then((data) => {
+			if (data.prof.message === "Not Found") {
+				//Show Alert
+				ui.showAlert("User not found", "alert alert-danger");
+			} else {
+				//Show Profile
+				ui.showProf(data.prof);
+				ui.showRepos(data.repos);
+				ui.clearAlert();
+			}
+		});
+	} else {
+		//Clear Profile
+		ui.clearProf();
+	}
+});
